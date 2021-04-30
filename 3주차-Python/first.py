33bb9171a8eccc9f09b5b13f5f0668468faca686
qkrthdus605 <qkrthdus605@gmail.com>
2021-04-30 23:40:56 +0900

    first commit

---
+++ 3주차-Python/first.py
@@ -0,0 +1,12 @@
+money = int(input("돈 얼마 있어? "))
+
+if(money>=5000):
+  print("아웃백 가자")
+else:
+  if(money>=3000):
+    print("학식 먹자")
+  else:
+    if(money>=1000):
+      print("컵라면 먹자")
+    else:
+      print("공기밥 가즈아ㅏ")
